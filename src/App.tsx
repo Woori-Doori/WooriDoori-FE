a0f43d5292a6d3c9fb5ceb89c78efb4055026ed7
ChatHongPT <suk924600@naver.com>
2025-10-05 18:01:20 +0900

    [feat] #12 - 불필요한 코드 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,31 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import styled from "styled-components";
-
+import CardManagement from './pages/CardManagement';
+import CardList from './pages/CardList';
+import CardDetail from './pages/CardDetail';
+import CardRegistration from './pages/CardRegistration';
 
 const AppContainer = styled.div`
   display: flex;
   flex-direction: column;
   min-height: 100vh;
-  align-items: center;
+  background-color: white;
+  width: 100%;
 `;
 
 const App: React.FC = () => {
   return (
-    <AppContainer>
-      
-    </AppContainer>
+    <Router>
+      <AppContainer>
+        <Routes>
+          <Route path="/" element={<CardManagement />} />
+          <Route path="/cards" element={<CardList />} />
+          <Route path="/cards/:id" element={<CardDetail />} />
+          <Route path="/cards/register" element={<CardRegistration />} />
+        </Routes>
+      </AppContainer>
+    </Router>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,10 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
-import App from "@/App";
-import { ThemeProvider } from "styled-components";
+import App from "./App";
 import React from "react";
-import Calendar from "./pages/Calender";
-
-const theme = {
-  colors: {
-    primary: "#3498db",
-    secondary: "#e74c3c",
-  },
-};
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-   <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<App />}>
-            <Route path="/calendar" element={<Calendar/>}/>
-            </Route>
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 );
