3fcf5351801aa53a13620fef6e7a2f30fd40f3b8
menzzi <minmin3927@naver.com>
2025-10-06 21:24:25 +0900

    [feat] #14 - 홈 페이지 추가

---
+++ src/pages/Home.tsx
@@ -0,0 +1,29 @@
+import TopIcons from "../components/TopIcons";
+import SideIcons from "../components/SideIcons";
+import DinoCharacter from "../components/DinoCharacter";
+import SpeechBox from "../components/SpeechBox";
+import Navbar from "../components/Navbar";
+
+const Home = () => {
+  return (
+    <div className="relative w-full h-screen bg-gradient-to-b from-sky-200 to-green-200 flex items-center justify-center overflow-hidden">
+      {/* 우측 상단: 알림/설정 */}
+      <TopIcons />
+      {/* 좌측 중앙: 게임/식사 */}
+      <SideIcons />
+
+      {/* 중앙: 두리 자리 */}
+      <DinoCharacter />
+
+      {/* 하단 메뉴 바로 위: 메시지 박스 */}
+      <div className="absolute bottom-20 w-full flex justify-center">
+        <SpeechBox text="메세지 박스 자리" />
+      </div>
+
+      {/* 하단 네비게이션 */}
+      <Navbar />
+    </div>
+  );
+};
+
+export default Home;
