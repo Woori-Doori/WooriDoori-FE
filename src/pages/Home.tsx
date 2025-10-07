02513dc0972881ba39504a6f5903b6acd24df664
menzzi <minmin3927@naver.com>
2025-10-07 19:56:45 +0900

    [feat] #14 - 홈 화면에 설정 기능 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,27 +3,32 @@ import SideIcons from "../components/SideIcons";
 import DinoCharacter from "../components/DinoCharacter";
 import SpeechBox from "../components/SpeechBox";
 import Navbar from "../components/Navbar";
+import SettingsModal from "../components/SettingsModal";
+import { useState } from "react";
 
 const Home = () => {
-  return (
-    <div className="relative w-full h-screen bg-gradient-to-b from-sky-200 to-green-200 flex items-center justify-center overflow-hidden">
-      {/* 우측 상단: 알림/설정 */}
-      <TopIcons />
-      {/* 좌측 중앙: 게임/식사 */}
-      <SideIcons />
+    const [showSettings, setShowSettings] = useState(false);
 
-      {/* 중앙: 두리 자리 */}
-      <DinoCharacter />
+    return (
+        <div className="relative w-full h-screen bg-gradient-to-b from-sky-200 to-green-200 flex items-center justify-center overflow-hidden">
+            {/* 우측 상단: 알림/설정 */}
+            <TopIcons onSettingsClick={() => setShowSettings(true)} />
+            {/* 좌측 중앙: 게임/식사 */}
+            <SideIcons />
 
-      {/* 하단 메뉴 바로 위: 메시지 박스 */}
-      <div className="absolute bottom-20 w-full flex justify-center">
-        <SpeechBox text="메세지 박스 자리" />
-      </div>
+            {/* 중앙: 두리 자리 */}
+            <DinoCharacter />
 
-      {/* 하단 네비게이션 */}
-      <Navbar />
-    </div>
-  );
+            {/* 하단 메뉴 바로 위: 메시지 박스 */}
+            <div className="absolute bottom-20 w-full flex justify-center">
+                <SpeechBox text="메세지 박스 자리" />
+            </div>
+
+            {/* 하단 네비게이션 */}
+            <Navbar />
+            {showSettings && <SettingsModal onClose={() => setShowSettings(false)} />}
+        </div>
+    );
 };
 
 export default Home;
