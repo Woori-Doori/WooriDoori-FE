d09d477238e79a1bdc9a6ad5d10b09c2e91f732d
Jsumin07 <sufisa.notion@gmail.com>
2025-10-08 23:52:56 +0900

    [feat] #17 - 아이디, 비밀번호 입력 필드 및 로그인 버튼 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-
+import Login from "./pages/Login";
 
 const AppContainer = styled.div`
   display: flex;
@@ -11,7 +11,7 @@ const AppContainer = styled.div`
 const App: React.FC = () => {
   return (
     <AppContainer>
-      
+      <Login />
     </AppContainer>
   );
 };
